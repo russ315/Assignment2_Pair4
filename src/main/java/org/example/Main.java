5b66b66592a714e5f3c1761e13aaca2d53880dbe
Alibi-stack <afox01155@gmail.com>
2025-10-05 12:52:16 +0500

    Student A task: - Added Binomial Min-Heap implementation - Added decrease-key and merge - Integrated with metrics and Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,47 @@ package org.example;
 
 import metrics.CsvExporter;
 import metrics.PerformanceTracker;
+import org.example.heap.BinomialHeap;
+import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        int n = 20000;
+        String dataType = "random";
         PerformanceTracker tracker = new PerformanceTracker();
+        BinomialHeap heap = new BinomialHeap(tracker);
+
+        int[] data = new int[n];
+        Random rnd = new Random(42);
+        for (int i = 0; i < n; i++) data[i] = rnd.nextInt(1_000_000);
+
+        tracker.reset();
         tracker.start();
+        BinomialHeap.Node[] handles = new BinomialHeap.Node[n];
+        for (int i = 0; i < n; i++) {
+            handles[i] = heap.insert(data[i]);
+        }
 
-        // Some work
-        Thread.sleep(2000);
+        for (int i = 0; i < n/100; i++) {
+            int idx = rnd.nextInt(n);
+            heap.decreaseKey(handles[idx], data[idx]/2);
+        }
 
+        int last = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int v = heap.extractMin();
+            if (v < last) throw new AssertionError("Heap order violated");
+            last = v;
+        }
         tracker.stop();
+
         CsvExporter exporter = new CsvExporter("results.csv");
-        exporter.export("MinHeap", 10000, "random",
+        exporter.export("BinomialMinHeap", n, dataType,
                 tracker.getComparisons(),
                 tracker.getSwaps(),
                 tracker.getDurationMs());
+
+        System.out.printf("Done. Comparisons=%d, Swaps=%d, Time=%.2f ms\n",
+                tracker.getComparisons(), tracker.getSwaps(), tracker.getDurationMs());
     }
 }
